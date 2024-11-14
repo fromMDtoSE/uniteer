3da49a31dc2577b0428384aba1b1493aa5322192
fromMDtoSE <raedigcse@gmail.com>
2024-11-14 09:03:29 -0800

    modifications

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -17,7 +17,7 @@ const ComingSoonPage = () => {
       <article className="text-info">
         <h2>We're</h2>
         <h2>Coming<br/>Soon</h2>
-        <p>Hello friends! We are currently building our new charity website. Stay tuned for more!</p>
+        <p>Hello friends! We are currently building our new website. Stay tuned for more!</p>
       </article>
       <div className="hero-image-desktop">
     <img src={heroImage} alt="https://www.vecteezy.com/free-photos/coming-soon Coming Soon Stock photos by Vecteezy"/>
